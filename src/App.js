9a362db4a7f6c57d4ace260ca36c6a967e1704d4
Yoav Shaked <yoavcad@gmail.com>
2020-09-21 00:29:40 +0300

    bla

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { FixedSizeList as List } from "react-window";
 import InfiniteLoader from "react-window-infinite-loader";
+import { debounce } from "lodash";
 
 export default function App() {
+  const [minPage, setMinPage] = React.useState(null);
   let page = 1;
   const LOADING = 1;
   const LOADED = 2;
   let itemStatusMap = {};
+
+  React.useEffect(() => {
+    const { pathname, origin } = window.location;
+
+    minPage &&
+      window.history.pushState(
+        { path: `${origin}${pathname}?page=${minPage.pageNumber}` },
+        "",
+        `${origin}${pathname}?page=${minPage.pageNumber}`
+      );
+  }, [minPage]);
 
   const isItemLoaded = (index) => !!itemStatusMap[index];
 
@@ -22,32 +35,54 @@ export default function App() {
           itemStatusMap[index].page = page;
         }
         resolve();
-      }, 2500)
+      }, 500)
     );
   };
 
-  const Row = ({ index, style }) => {
+  const createRow = ({ index, style }) => {
+    const rowRef = React.useRef();
     let label;
     if (itemStatusMap[index]?.loading === LOADED) {
-        console.log('page', itemStatusMap[index])
-        console.log('index', index)
-       updateUrl(index);
       label = `Row ${index} Page ${itemStatusMap[index]?.page}`;
+    //  updateUrl(itemStatusMap, index);
     } else {
       label = "Loading...";
     }
+    const positionY = rowRef?.current?.getBoundingClientRect().y;
+    if (!minPage) {
+      setMinPage({positionY, pageNumber: itemStatusMap[index]?.page});
+    } else if (positionY < minPage.positionY) {
+      setMinPage({positionY, pageNumber: itemStatusMap[index]?.page});
+    }
     return (
       <div
+        key={`${itemStatusMap[index]?.page}-${index}`}
         id={`${itemStatusMap[index]?.page}-${index}`}
         className="ListItem"
         style={style}
+        ref={rowRef}
       >
         {label}
       </div>
     );
   };
 
-  const onScroll = (e) => {};
+  const listComponent = ({ onItemsRendered, ref }) => {
+    return (
+      <List
+        // onScroll={debounce(onScroll, 200)}
+        className="List"
+        height={300}
+        itemCount={1000}
+        itemSize={50}
+        onItemsRendered={onItemsRendered}
+        ref={ref}
+        width={500}
+      >
+        {createRow}
+      </List>
+    );
+  };
 
   return (
     <InfiniteLoader
@@ -55,37 +90,20 @@ export default function App() {
       itemCount={1000}
       loadMoreItems={loadMoreItems}
     >
-      {({ onItemsRendered, ref }) => (
-        <List
-          onScroll={onScroll}
-          className="List"
-          height={150}
-          itemCount={1000}
-          itemSize={30}
-          onItemsRendered={onItemsRendered}
-          ref={ref}
-          width={300}
-        >
-          {Row}
-        </List>
-      )}
+      {listComponent}
     </InfiniteLoader>
   );
+}
 
-    function updateUrl(index) {
-        const { search, pathname, origin } = window.location;
-        if (!search) {
-            history.pushState(
-                { path: `${origin}${pathname}?page=1` },
-                "",
-                `${origin}${pathname}?page=1`
-            );
-        } else {
-            history.pushState(
-                { path: `${origin}${pathname}?page=${itemStatusMap[index]?.page}` },
-                "",
-                `${origin}${pathname}?page=${itemStatusMap[index]?.page}`
-            );
-        }
-    }
+// TODO: spy list children in order to know wich is the first one
+function updateUrl(itemStatusMap, index) {
+  const { pathname, origin } = window.location;
+
+  window.history.pushState(
+    { path: `${origin}${pathname}?page=${itemStatusMap[index]?.page}` },
+    "",
+    `${origin}${pathname}?page=${itemStatusMap[index]?.page}`
+  );
+  console.log("page", itemStatusMap[index]);
+  console.log("index", index);
 }
